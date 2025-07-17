08a8379e206e13c165e058e325e90f728245bed9
meentho <meenttoo@gmail.com>
2025-07-17 09:31:28 +0900

    feat: 로그인 화면 UI 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ArsAuth from '../pages/ars/ArsAuth.vue';
 import ArsVerification from '../pages/ars/ArsVerification.vue';
+import Login from '../pages/Login.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/ars-auth',
       name: 'ARSAuth',
       component: ArsAuth,
     },
