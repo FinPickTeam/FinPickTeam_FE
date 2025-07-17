e867587b1317699cbe05554d72a25222cdfde21e
meentho <meenttoo@gmail.com>
2025-07-17 13:45:39 +0900

    feat: 회원가입 페이지 구현 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ArsAuth from '../pages/ars/ArsAuth.vue';
 import ArsVerification from '../pages/ars/ArsVerification.vue';
-import Login from '../pages/Login.vue';
+import Login from '../pages/auth/Login.vue';
+import Signup from '../pages/auth/Signup.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       component: Login,
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup,
     },
     {
       path: '/ars-auth',
