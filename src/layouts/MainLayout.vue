d46eeca6e1066288b9fdc3e8445085d52f8ec2a9
meentho <meenttoo@gmail.com>
2025-07-25 10:32:41 +0900

    feat:  financehome에서만 배경색 따로  적용

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,9 @@
   <div class="main-layout">
     <!-- dictionary, notification 경로에서는 헤더 숨김 -->
     <HeaderBar v-if="shouldHideHeader" />
-    <main class="main-content">
+    <main
+      :class="['main-content', { 'finance-bg': route.name === 'FinanceHome' }]"
+    >
       <router-view />
     </main>
     <Navbar />
@@ -10,23 +12,24 @@
 </template>
 
 <script setup>
-import HeaderBar from "@/components/Headerbar.vue";
-import Navbar from "@/components/Navbar.vue";
-import { useRoute } from "vue-router";
-import { computed } from "vue";
+import HeaderBar from '@/components/Headerbar.vue';
+import Navbar from '@/components/Navbar.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
 
 const route = useRoute();
 // HeaderBar를 숨길 라우트 이름 목록
 // (이 배열에 포함된 name의 페이지에서는 HeaderBar가 표시되지 않음)
 const hideHeaderNames = [
-  "dictionary",
-  "Notification",
+  'dictionary',
+  'Notification',
 
-  "profile",
-  "quiz-history",
-  "withdraw",
-  "avatar-shop",
-  "ChallengeHome",
+  'profile',
+  'quiz-history',
+  'withdraw',
+  'avatar-shop',
+
+  'ChallengeHome',
 ];
 const shouldHideHeader = computed(() => {
   return !hideHeaderNames.includes(route.name);
@@ -43,5 +46,8 @@ const shouldHideHeader = computed(() => {
 .main-content {
   flex: 1 0 auto;
   padding-bottom: 80px; /* 하단 네비게이션 영역 확보 */
+}
+.finance-bg {
+  background: var(--color-bg-light);
 }
 </style>
