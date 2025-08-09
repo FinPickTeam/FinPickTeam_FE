e27aedf3b74d0bf2ded38e5c04dd2d48148bf422
meentho <meenttoo@gmail.com>
2025-08-10 00:29:46 +0900

    Style: 재테크 상품 리스트 gap 조정

--- src/components/finance/deposit/ProductCardList_deposit.vue
+++ src/components/finance/deposit/ProductCardList_deposit.vue
@@ -23,7 +23,7 @@ const props = defineProps({
 .product-card-list {
   display: flex;
   flex-direction: column;
-  gap: 0px;
+  gap: 10px;
   align-items: center;
   justify-content: center;
   padding: 20px;
