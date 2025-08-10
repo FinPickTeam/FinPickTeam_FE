7c07d3ad383405b332ff617d79e12788a35ac534
chojayoung <26006@naver.com>
2025-08-10 11:06:23 +0900

    fix: 사용자 로그인시 유저 정보 및 닉네임 영구 저장하도록 플러그인 등록

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { setupCalendar } from 'v-calendar';
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
@@ -9,6 +11,9 @@ import 'v-calendar/style.css';
 
 const app = createApp(App);
 const pinia = createPinia();
+
+// ★ Pinia 영구 저장 플러그인 연결
+pinia.use(piniaPluginPersistedstate);
 
 app.use(setupCalendar, { componentPrefix: 'V' });
 app.use(pinia);
